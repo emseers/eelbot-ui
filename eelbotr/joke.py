42b32fa8ed68ab7b01578b043a8370ba3c7c089a
Author Name <ahmedaashkir@gmail.com>
2019-08-12 18:54:56 -0400

    added basic backend

---
+++ eelbotr/joke.py
@@ -0,0 +1,28 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, request, session, url_for, jsonify
+)
+
+from eelbotr.db import get_db, query_db
+
+# this is the joke blueprint, define endpoints for jokes here
+
+bp = Blueprint('joke', __name__, url_prefix='/joke')
+
+# view/endpoint for requesting all jokes
+@bp.route('/')
+def jokes():
+    all_jokes = {}
+    db = get_db()
+    for joke in query_db('select * from EelJokes'):
+        joke_obj = {'JokeID' : None, 'JokeText' : None, 'JokeTextLine2' : None}
+        joke_obj['JokeID'] = joke['JokeID']
+        joke_obj['JokeText'] = joke['JokeText']
+        joke_obj['JokeTextLine2'] = joke['JokeTextLine2']
+
+        all_jokes[joke['JokeID']] = joke_obj
+
+    return jsonify(all_jokes)
+
+# add other joke endpoints here
