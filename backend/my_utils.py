fc78e920cf79b33263a2f1d48e3b06e92ec71549
Sriram Suresh <sriram.suresh@live.ca>
2020-09-06 18:23:32 -0600

    Delete old stuff

--- backend/my_utils.py
+++
@@ -1,8 +0,0 @@
-from flask import (
-    request, jsonify
-)
-
-def package_response(response_obj):
-    response = jsonify(response_obj)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
