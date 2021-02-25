6d7c635c9f42f380e2ca6ed875d49b5f44d7c435
Sriram Suresh <sriram.suresh@live.ca>
2021-02-25 10:56:23 -0700

    Update eeljokes.py to remove temp variable and return directly
    Co-authored-by: Adipa Wijayathilaka <adipamihi@gmail.com>

--- backend/utils/eeljokes.py
+++ backend/utils/eeljokes.py
@@ -10,5 +10,4 @@ class EelJokes(db.Model):
     JokeTextLine2 = db.Column(db.Text, nullable=True)
 
     def get_joke_data(self):
-        joke_data = {"JokeID": self.JokeID, "JokeText": self.JokeText, "JokeTextLine2": self.JokeTextLine2}
-        return joke_data
+        return {"JokeID": self.JokeID, "JokeText": self.JokeText, "JokeTextLine2": self.JokeTextLine2}
