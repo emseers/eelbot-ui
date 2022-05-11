47628c64deca56000120eca2aaad1f3c7713435e
aashkir <ahmedaashkir@gmail.com>
2022-05-11 17:20:37 -0400

    Fix: Single quote accept header

--- frontend/src/services/jokes.js
+++ frontend/src/services/jokes.js
@@ -24,7 +24,7 @@ const create = async (joke) => {
     const settings = {
         method: 'POST',
         headers: {
-            Accept: 'application/json',
+            'Accept': 'application/json',
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(joke)
@@ -39,7 +39,7 @@ const put = async (id, joke) => {
     const settings = {
         method: 'PUT',
         headers: {
-            Accept: 'application/json',
+            'Accept': 'application/json',
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(joke)
